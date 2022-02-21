667dfd3cfa641c440f23c5a5b4c5ff72b23381fc
Marco Luzzara <marco.luzzara@hotmail.it>
2022-02-21 15:46:39 +0100

    session is stored inside db
    - add runtime.txt file to let heroku know the correct python version
    - change regex to match both 'errore' and 'errori'
    - configure postgres db for session storing

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 from __future__ import annotations
-from cProfile import run
 from telethon import TelegramClient, events
 import re
 import os
 import logging
-logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-                    level=logging.WARNING)
-
-best_deals_pattern = re.compile(r'.*\berrore\b.*', re.I)
+from alchemysession import AlchemySessionContainer
 
 # set the environment variables below in a file called .env (for example)
 #
@@ -23,8 +19,19 @@ api_id = int(os.environ['API_ID'])
 api_hash = os.environ['API_HASH']
 accepted_entities = os.environ['ACCEPTED_ENTITIES'].split(':')
 entity_to_forward = os.environ['ENTITY_TO_FORWARD']
+db_conn_string = 'postgresql' + os.environ['DATABASE_URL'][8:]
 
-with TelegramClient('anon', api_id, api_hash) as client:
+# session initialization
+container = AlchemySessionContainer(db_conn_string)
+session = container.new_session('main')
+
+# logging
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+
+best_deals_pattern = re.compile(r'.*\berror[ei]\b.*', re.I)
+
+with TelegramClient(session, api_id, api_hash) as client:
     client: TelegramClient
     @client.on(events.NewMessage(\
         incoming=True, forwards=False, pattern=best_deals_pattern, from_users=accepted_entities))
